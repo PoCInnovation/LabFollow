41d3ec60e4b6c9d9d191e7035f1fca3f1c1ce60d
Paul Monnery <paul.monnery@epitech.eu>
2019-11-12 16:40:42 +0100

    [REMOVED] useless node modules

--- App.js
+++ App.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import { StatusBar } from 'react-native'
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
-import { uri as serverURL } from './src/network'
 import { Connection } from './src/navigator/navigator'
 import Provider from "./src/store/provider";
 
 StatusBar.setBarStyle('light-content', true);
 
-const client = new ApolloClient({
-  uri: serverURL,
-});
-
-
 const App = () => {
   return (
-    <ApolloProvider client={client}>
-      <Provider>
-        <Connection />
-      </Provider>
-    </ApolloProvider>
+    <Provider>
+      <Connection />
+    </Provider>
   );
 }
 
